ce7c87f2d8adc25c13525f66b1a9ff5c46fc61af
Avijit <abhijitpradhan909@gmail.com>
2024-12-11 18:43:34 +0530

    adding gujarati more categoty

--- utils/constant.js
+++ utils/constant.js
@@ -1,3 +1,3 @@
 export const questionLevels = ["Beginner", "Intermediate", "Advanced"]
 
-export const topics = ['Alphabet', 'Number', "Greetings", "Mathematics", "Body Parts", "Colors", "Food", "Transport", "Animals", "Science", "Emotions", "Shapes", "Operators", 'Basic Calculation', 'Gujarati Alphabets'];
+export const topics = ['Alphabet', 'Number', "Greetings", "Mathematics", "Body Parts", "Colors", "Food", "Transport", "Animals", "Science", "Emotions", "Shapes", "Operators", 'Basic Calculation', 'Gujarati Alphabets', "Gujarati Numbers", "Gujarati Vehicles"];
